f446917242120aaf8a2828ac853eb94df2cb7a69
Robin <midouesterner@gmail.com>
2022-08-06 07:44:42 -0500

    remove Loader struct

--- lib/dpdc/loader.ex
+++ lib/dpdc/loader.ex
@@ -1,22 +1,21 @@
 defmodule DPDC.Loader do
-  defstruct [
-    :bytes,
-    :size_upvalues,
-    :proto
-  ]
-
-  alias __MODULE__, as: Loader
   alias DPDC.Loader.Base, as: Base
   alias DPDC.Loader.Header, as: Header
   alias DPDC.Loader.Proto, as: Proto
 
   def load(<<bytes::binary>>) do
     rest = Header.assert(bytes)
-
-    {size_upvalues, rest} = Base.load_byte(rest)
-
+    {sizeupvalues, rest} = Base.load_byte(rest)
     {proto, rest} = Proto.load(rest)
 
-    %Loader{bytes: rest, size_upvalues: size_upvalues, proto: proto}
+    if sizeupvalues != length(proto.upvalues) do
+      raise("Incorrect number of upvalues")
+    end
+
+    if rest != "" do
+      raise("Not all bytes consumed")
+    end
+
+    proto
   end
 end
