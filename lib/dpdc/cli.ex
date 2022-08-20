ac27d8c29c415db3b0f304445e4a132571e86208
Robin <midouesterner@gmail.com>
2022-08-20 11:24:01 -0500

    add --colors/-c cli switch

--- lib/dpdc/cli.ex
+++ lib/dpdc/cli.ex
@@ -1,31 +1,39 @@
 defmodule DPDC.CLI do
   def main(argv \\ []) do
-    {_, args} = OptionParser.parse!(argv, switches: [])
+    {parsed, rest} =
+      OptionParser.parse!(argv,
+        switches: [colors: :boolean],
+        aliases: [c: :colors]
+      )
 
-    case args do
+    case rest do
       [path] ->
         proto =
           path
           |> DPDC.Load.load_file!()
           |> DPDC.Disassemble.load_proto!()
 
-        IO.inspect(proto,
-          limit: :infinity,
-          pretty: true,
-          syntax_colors: [
-            atom: :cyan,
-            binary: :white,
-            boolean: :magenta,
-            list: :white,
-            map: :white,
-            number: :yellow,
-            string: :green,
-            tuple: :white
-          ]
-        )
+        IO.inspect(proto, inspect_opts(parsed))
 
       _ ->
         IO.puts("Usage: dpdc <example.luac>")
     end
+  end
+
+  defp inspect_opts([]), do: [limit: :infinity, pretty: true]
+
+  defp inspect_opts(colors: true) do
+    Keyword.merge(inspect_opts([]),
+      syntax_colors: [
+        atom: :cyan,
+        binary: :white,
+        boolean: :magenta,
+        list: :white,
+        map: :white,
+        number: :yellow,
+        string: :green,
+        tuple: :white
+      ]
+    )
   end
 end
